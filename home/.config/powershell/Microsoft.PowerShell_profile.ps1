2da2462534d65cffc179f5e46d480307b10d00a8
Steven "NemesisRE" Kurz <nemesissre@gmail.com>
2024-07-16 17:49:55 +0200

    refactored and updated

--- home/.config/powershell/Microsoft.PowerShell_profile.ps1
+++ home/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -3,61 +3,59 @@ Set-Alias k -Value kubectl
 Set-Alias kns -Value Select-KubeNamespace
 Set-Alias kctx -Value Select-KubeContext
 
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+
 # Set Profile Path
-#$PROFILE_PATH = Split-Path -Path ${PROFILE}
-$SCRIPT_PATH = Split-Path -Parent $MyInvocation.MyCommand.Definition
+#${PROFILE_PATH} = Split-Path -Path ${PROFILE}
+${SCRIPT_PATH} = Split-Path -Parent $MyInvocation.MyCommand.Definition
 # Source functions
 . "${SCRIPT_PATH}\Profile_Functions.ps1"
 . "${SCRIPT_PATH}\Kubectl_Completion.ps1"
 
-# Define modules to be used
-# ${MODULES} = @(
-#   'PSReadLine'
-#   , 'oh-my-posh'
-#   , 'posh-git'
-#   , 'Pscx'
-#   , 'PSFzf'
-#   , 'Recycle'
-#   , 'Terminal-Icons'
-#   , 'GuiCompletion'
-#   , 'PSKubeContext'
-# )
-
-${MODULES} = @(
-  'PSReadLine'
-  , 'oh-my-posh'
-  , 'posh-git'
-  , 'PSFzf'
-  , 'Recycle'
-  , 'Terminal-Icons'
-  , 'GuiCompletion'
-  , 'PSKubeContext'
-  , 'ZLocation'
-  , 'PoShLog'
-)
+${MODULES} = New-Object System.Collections.ArrayList
+[Void]${MODULES}.Add("PSReadLine")
+#[Void]${MODULES}.Add("oh-my-posh")
+[Void]${MODULES}.Add("posh-git")
+[Void]${MODULES}.Add("PSFzf")
+[Void]${MODULES}.Add("Recycle")
+[Void]${MODULES}.Add("Terminal-Icons")
+[Void]${MODULES}.Add("PSKubeContext")
+[Void]${MODULES}.Add("ZLocation")
+[Void]${MODULES}.Add("PoShLog")
 
 if ( ${isWindows} ) {
   ${ENV:PATH} += ";${HOME}\.local\bin"
+  [Void]${MODULES}.Add("GuiCompletion")
 } elseif ( ${isLinux} ) {
   ${ENV:PATH} += ";${HOME}/.local/bin"
-  ${MODULES}.Remove('GuiCompletion')
 }
 
-#Update-Module
+if (Test-Path "${SCRIPT_PATH}\Local_Modules.ps1") {
+  . "${SCRIPT_PATH}\Local_Modules.ps1"
+}
+
+Update-Module
 
 # Load Modules and install if necessary
-foreach (${MODULE} in ${MODULES}) { LoadModule ${MODULE} }
+foreach (${MODULE} in ${MODULES}) {
+  LoadModule ${MODULE}
+}
 
 # Set Powershell Theme
-Set-PoshPrompt -Theme powerlevel10k_rainbow
+if (Get-Command oh-my-posh -errorAction SilentlyContinue) {
+  oh-my-posh init pwsh --config "$ENV:POSH_THEMES_PATH\powerlevel10k_rainbow.omp.json" | Invoke-Expression
+}
 
 # Set PSReadLine options
 Set-PSReadLineOption -PredictionSource History
 
 # Set Keyhandlers
 #Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-GuiCompletion }
-#Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion -CaseInsensitive}
+if ( ${isWindows} ) {
+  Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-GuiCompletion }
+} elseif ( ${isLinux} ) {
+  Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion -CaseInsensitive }
+}
 Set-PSReadLineKeyHandler -Key Ctrl+d -Function ViExit
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
@@ -75,7 +73,7 @@ $MaximumHistoryCount = 10000;
 # PSFzf settings
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 
-# Reload $env:PATH
+# Reload $ENV:PATH
 ${ENV:PATH} = [System.Environment]::GetEnvironmentVariable('Path', 'Machine') + ';' + [System.Environment]::GetEnvironmentVariable('Path', 'User')
 
 # Register Completions
